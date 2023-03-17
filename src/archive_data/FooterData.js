7510c4d21cd263201d4f4414805f352c4716f089
MuhammadRabius <muhammadrabiussany@gmail.com>
2023-03-17 15:28:30 +0600

    Footer Complte

---
+++ src/archive_data/FooterData.js
@@ -0,0 +1,231 @@
+import { FaFacebook, FaTwitter, FaYoutube, FaInstagram } from "react-icons/fa";
+
+export default function FooterData() {
+  return [
+    {
+      title: "Trending",
+      list: [
+        {
+          link: "/",
+          title: "Installments",
+        },
+
+        {
+          link: "/",
+          title: "Electronics",
+        },
+
+        {
+          link: "/",
+          title: "Health & Beauty",
+        },
+        {
+          link: "/",
+          title: "Home Appliances",
+        },
+        {
+          link: "/",
+          title: "Mobile Accessories",
+        },
+      ],
+    },
+
+    {
+      title: "Information",
+      list: [
+        {
+          link: "/",
+          title: "About Us",
+        },
+
+        {
+          link: "/",
+          title: "Contact Us",
+        },
+
+        {
+          link: "/",
+          title: "FAQs",
+        },
+
+        {
+          link: "/",
+          title: "Shipping & Return",
+        },
+        {
+          link: "/",
+          title: "Privacy policy",
+        },
+        {
+          link: "/",
+          title: "Terms & Conditions",
+        },
+      ],
+    },
+
+    //     {
+    //       title: "Customer Care",
+    //       list: [
+    //         {
+    //           link: ``,
+    //           title: "Facebook",
+    //           icon: <FaFacebook />,
+    //         },
+
+    //         {
+    //           link: ``,
+    //           title: "Twitter",
+    //           icon: <FaTwitter />,
+    //         },
+
+    //         {
+    //           link: ``,
+    //           title: "Instagram",
+    //           icon: <FaInstagram />,
+    //         },
+
+    //         {
+    //           link: ``,
+    //           title: "Youtube",
+    //           icon: <FaYoutube />,
+    //         },
+    //       ],
+    //     },
+
+    {
+      title: "Customer Care",
+      list: [
+        {
+          link: "/",
+          title: "My Account",
+        },
+
+        {
+          link: "/",
+          title: "Track Your Order",
+        },
+
+        {
+          link: "/",
+          title: "Recently Viewed",
+        },
+
+        {
+          link: "/",
+          title: "Wishlist",
+        },
+        {
+          link: "/",
+          title: "Compare",
+        },
+        {
+          link: "/",
+          title: "Become a Vendor",
+        },
+      ],
+    },
+  ];
+}
+
+// export const footer_list = [
+
+//     {
+//         title: "Product",
+//         list: [
+//             {
+//                 link: "/",
+//                 title: "Flights",
+//             },
+
+//             {
+//                 link: "/",
+//                 title: "Loan",
+//             },
+
+//             {
+//                 link: "/",
+//                 title: "Insurance",
+//             },
+//         ]
+//     },
+
+//     {
+//         title: "About My Sawari",
+//         list: [
+//             {
+//                 link: "/howtobook",
+//                 title: "How to Book",
+//             },
+
+//             {
+//                 link: "/help",
+//                 title: "Help",
+//             },
+
+//             {
+//                 link: "/aboutus",
+//                 title: "About us",
+//             },
+
+//             {
+//                 link: "/contactus",
+//                 title: "Contact US",
+//             },
+//         ]
+//     },
+
+//     {
+//         title: "Follow us on",
+//         list: [
+//             {
+//                 link: "/",
+//                 title: "Facebook",
+//                 icon: <FaFacebook />
+//             },
+
+//             {
+//                 link: "/",
+//                 title: "Twitter",
+//                 icon: <FaTwitter />
+//             },
+
+//             {
+//                 link: "/",
+//                 title: "Instagram",
+//                 icon: <FaInstagram />
+//             },
+
+//             {
+//                 link: "/",
+//                 title: "Youtube",
+//                 icon: <FaYoutube />
+//             },
+
+//         ]
+//     },
+
+//     {
+//         title: "Others",
+//         list: [
+//             {
+//                 link: "/blog",
+//                 title: "Blog",
+//             },
+
+//             {
+//                 link: "/privacypolicy",
+//                 title: "Privacy Policy",
+//             },
+
+//             {
+//                 link: "/termscondition",
+//                 title: "Terms & Conditions",
+//             },
+
+//             {
+//                 link: "/refundpolicy",
+//                 title: "Refund policy",
+//             },
+//         ]
+//     },
+// ]
--- src/component/Footer/Footer.js
+++ src/component/Footer/Footer.js
@@ -1,22 +1,84 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
+import FooterData from "../../archive_data/FooterData";
+import {
+  AiOutlineFacebook,
+  AiOutlineTwitter,
+  AiFillLinkedin,
+  AiOutlineInstagram,
+} from "react-icons/ai";
 import "./Footer.scss";
 
 const Footer = () => {
   return (
-    <>
-      <div className="footer_page">
-        <div className="container">
-          {/* Footer text */}
-          <div>
-            <span className="footer_text">StayIn</span> <br />
-            <span className="footer_text">touch with</span> <br />
-            <span className="footer_text">Mediusware</span>
+    <div className="footer">
+      <div className="container">
+        <div className="footer__row">
+          <div className="row">
+            <div className="footer__logo">
+              <h1 className="c_title">Win Store</h1>
+              <span className="title">Got questions? Call us 24/7!</span>
+            </div>
+
+            <div className="com_mob">
+              <span>
+                03 111 666 144 <br /> 0317 1777015.
+              </span>
+            </div>
+
+            <div className="com_con">
+              <span className="title">Contact info </span> <br />
+              <span>info@winstore.pk</span>
+            </div>
+
+            {/* certified logo*/}
+            <div className="certified__logos row__logos">
+              <div className="s__logos">
+                <AiOutlineFacebook />
+                <AiOutlineTwitter />
+                <AiFillLinkedin />
+                <AiOutlineInstagram />
+              </div>
+            </div>
           </div>
-          <hr className="mt-2" />
+
+          {/* row---------  */}
+          {FooterData().map((item, i) => {
+            return (
+              <div className="row footer__navs" key={i}>
+                <h1 className="title"> {item.title} </h1>
+
+                {/* menu list  */}
+                <div className="list">
+                  {item.list.map((list, indx) => {
+                    // console.log('list of menu', list);
+                    return (
+                      <ul key={indx}>
+                        <li>
+                          <a href={list.link ? list.link : "/"}>
+                            {list.icon && list.icon}
+                            {list.title}
+                          </a>
+                        </li>
+                      </ul>
+                    );
+                  })}
+                </div>
+              </div>
+            );
+          })}
+        </div>
+        {/* payments logo*/}
+        <div className="payments__logos row__logos">
+          <div className="t__logos">
+            <img src="/images/payments/mastercard.svg" alt="mastercard" />
+            <img src="/images/payments/visa.svg" alt="viaa" />
+
+            <img src="/images/payments/nagad.svg" alt="digicert" />
+            <img src="/images/payments/upay.svg" alt="digicert" />
+          </div>
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
